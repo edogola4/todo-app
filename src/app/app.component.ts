ad4c0632da01adc5f03617979f90533773d81441
edogola4 <edogola4@gmail.com>
2025-09-09 12:06:17 +0300

    refactor: remove theme toggle and related code
    - Removed theme toggle component and service
    - Reverted app component styles to original
    - Cleaned up theme-related code and dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodoComponent } from './components/todo/todo.component';
-import { ThemeToggleComponent } from './components/theme-toggle/theme-toggle.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, TodoComponent, ThemeToggleComponent],
+  imports: [CommonModule, TodoComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
